bc39ff72409337052418d4f7974b9136bdc4af6d
Fernando <suporteiros@gmail.com>
2024-10-01 22:32:23 -0300

    Context menu ready

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/ContactActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/ContactActivity.kt
@@ -2,10 +2,12 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.GONE
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityContactBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class ContactActivity : AppCompatActivity() {
@@ -21,12 +23,37 @@ class ContactActivity : AppCompatActivity() {
         setSupportActionBar(acb.toolbarIn.toolbar)
         supportActionBar?.subtitle = getString(R.string.contact_details)
 
+        //Precisamos veririfcar se a intent que chamou essa activity recebeu algum extra quando chamou essa intent de abertura .
+        val receivedContact = intent.getParcelableExtra<Contact>(EXTRA_CONTACT)
+        //se por acaso esse received contact for diferente de nulo, eu posso usar esses dados para preencher os meus campos de formulário.
+        receivedContact?.let { received ->
+            //verificar se recebeu mais algum extra além do contato.
+            val viewContact = intent.getBooleanExtra(EXTRA_VIEW_CONTACT, false)
+            with(acb){
+                if (viewContact) {
+                    nameEt.isEnabled = false
+                    addressEt.isEnabled = false
+                    phoneEt.isEnabled = false
+                    emailEt.isEnabled = false
+                    saveBt.visibility = GONE
+                }
+                nameEt.setText(received.name)
+                addressEt.setText(received.address)
+                phoneEt.setText(received.phone)
+                emailEt.setText(received.email)
+            }
+        }
+
+
         //adicionar um listenet ao botão save. Criei com o with pois vou ter que acessar vários elementos da view
         with(acb){
             saveBt.setOnClickListener {
                 //criar um contato com os dados que foram preenchidos no formulário
                 val contact = Contact(
-                    id = hashCode(),
+                    //Aqui eu posso tanto estar editando um contato como recebendo um contato novo. Se eu estiver editando um contato eu vou receber o id dele.
+                    //Se ele estiver nulo, significa que eu estou criando um novo contato. Nesse caso, eu posso usar o hashCode() para gerar um id único.
+                    //Por isso eu usei o operador elvis ?: para verificar se o id é nulo. Se for nulo, ele vai usar o hashCode() para gerar um id.
+                    id = receivedContact?.id?:hashCode(),
                     name = nameEt.text.toString(),
                     address = addressEt.text.toString(),
                     phone = phoneEt.text.toString(),
--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -3,15 +3,20 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -52,12 +57,14 @@ class MainActivity : AppCompatActivity() {
                 //o getParcelableExtra é um método que pega um objeto que foi passado como parâmetro para a activity
                 val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
                 //aqui usei also ao invés de let pois precisei fazer duas coisas: adicionar o contato na lista e notificar o adapter
-                contact?.also {
-                     if(contactList.any{it.id == contact.id}) {
+                contact?.also { newOrEditedContact ->
+                     if(contactList.any{it.id == newOrEditedContact.id}) {
                          // Editar contato
+                         val position = contactList.indexOfFirst { it.id == newOrEditedContact.id }
+                         contactList[position] = newOrEditedContact
                      } else {
                          //se o contato já existe, atualiza ele
-                        contactList.add(contact)
+                        contactList.add(newOrEditedContact)
                      }
                     //preciso notificicar se o adpter foi alterado
                     contactAdapter.notifyDataSetChanged()
@@ -69,6 +76,37 @@ class MainActivity : AppCompatActivity() {
 
         //o contactsLv é a id fa ListView que criei no layout
         amb.contactsLv.adapter = contactAdapter
+
+        //Refere-se a uma ListView que está dentro do binding amb
+        //contactsLv é o ID da ListView dentro do layout XML, e amb.contactsLv permite que você acesse essa lista.
+        //Estamos recebendo o contactAdapter, é uma instância de ContactAdapter.
+        amb.contactsLv.adapter = contactAdapter
+        //registra a ListView (contactsLv) para que um context menu seja exibido quando um item da lista for pressionado e segurado por um tempo.
+        //registerForContextMenu(View): Esse diz ao sistema Android que a ListView (amb.contactsLv) tem um menu de contexto.
+        //Agora ele vai procurar pelos métodos onCreateContextMenu e onContextItemSelected para criar e tratar o menu de contexto.
+        registerForContextMenu(amb.contactsLv)
+        //Só para constar, coloque no onDestroy um unregisterForContextMenu(amb.contactsLv) para você desregistrar o menu de contexto.
+        // Veja mais no fim do código.
+
+
+        //Aqui eu criei um listener para o clique em um item da lista. O clique curto vai permitir visualizar o contato
+        amb.contactsLv.setOnItemClickListener { _, _, position, _ ->
+            //pega o contato que foi clicado
+            val contact = contactList[position]
+            //cria um intent para abrir a activity de contato
+            val viewContactIntent = Intent(this, ContactActivity::class.java)
+            //passa o contato como parâmetro para a activity
+            viewContactIntent.putExtra(EXTRA_CONTACT, contact)
+            viewContactIntent.putExtra(EXTRA_VIEW_CONTACT, true)
+            //inicia a activity dessa forma pois eu não preciso de um retorno dela.
+            startActivity(viewContactIntent)
+
+            //tudo numa chamada só
+//            startActivity(Intent(this, ContactActivity::class.java).apply {
+//                putExtra(EXTRA_CONTACT, contactList[position])
+//                putExtra(EXTRA_VIEW_CONTACT, contact)
+//            })
+        }
     }
 
     //override do método que cria o menu
@@ -92,6 +130,55 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+    }
+
+    //Aqui criamos o menu de contexto que vai aparecer ao fazer um long click em um item da lista
+    //Para que ele apareça, temos que associar adapter view ao menu de contexto. Isso eu chamo lá na onCreate
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?, menuInfo:
+        ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //infla o menu de contexto que criei no res/menu/context_menu_main.xml
+        menuInflater.inflate(R.menu.context_menu_main, menu)
+    }
+
+    //Esse é o corpo da função que trata as opções do menu de contexto.
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        //pega a posição do item que foi clicado
+        val position = (item.menuInfo as AdapterView.AdapterContextMenuInfo).position
+        return when(item.itemId){
+            R.id.removeContactMi -> {
+                //remove o item da lista
+                contactList.removeAt(position)
+                //notifica o adapter que a lista foi alterada
+                contactAdapter.notifyDataSetChanged()
+                //Avisa o usuário que o contato foi removido
+                Toast.makeText(this, getString(R.string.contato_removido), Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.editContactMi -> {
+                //pega o contato que foi clicado
+                val contact = contactList[position]
+                //cria um intent para abrir a activity de contato
+                val editContactIntent = Intent(this, ContactActivity::class.java)
+                //passa o contato como parâmetro para a activity
+                editContactIntent.putExtra(EXTRA_CONTACT, contact)
+                //inicia a activity
+                carl.launch(editContactIntent)
+                //fazendo tudo numa única linha fica
+    //carl.launch(Intent(this, ContactActivity::class.java).apply {putExtra(EXTRA_CONTACT, contactList[position])})
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterForContextMenu(amb.contactsLv)
     }
 
     private fun fillContats() {
